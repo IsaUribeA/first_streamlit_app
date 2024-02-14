d2ffc66993b465fc57d6ba7cda368ca4b1708443
Isabel Uribe <87680316+IsaUribeA@users.noreply.github.com>
2024-02-14 16:50:57 -0600

    Update streamlit_app.py
    pre-populate the list to set an example for the customer on how to choose the fruits

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,8 +15,10 @@ import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 my_fruit_list = my_fruit_list.set_index('Fruit') #agarra de la columna fruits los index para la lista
 
+#We want to filter the table data based on the fruits a customer will choose, so we'll pre-populate the list to set an example for the customer. 
+
 # Let's put a pick list here so they can pick the fruit they want to include 
-streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
+streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
 
 # Display the table on the page.
 streamlit.dataframe(my_fruit_list)

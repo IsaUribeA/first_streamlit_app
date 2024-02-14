5a600eeed308b95f8a7694bd9f5623261e260947
Isabel Uribe <87680316+IsaUribeA@users.noreply.github.com>
2024-02-14 17:01:39 -0600

    Update streamlit_app.py
    correcion linea 25 llamando a la variable correcta

--- streamlit_app.py
+++ streamlit_app.py
@@ -19,7 +19,7 @@ my_fruit_list = my_fruit_list.set_index('Fruit') #agarra de la columna fruits lo
 
 # Let's put a pick list here so they can pick the fruit they want to include 
 fruits_selected = streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index),['Avocado','Strawberries'])
-fruits_to_show = my_fruit_list.loc[fruits_selected]
+fruits_to_show = my_fruit_list.loc[fruits_selected] #agarra las frutas seleccionadas y las jala del full data set a otra variable llamada fruits to show
 
 # Display the table on the page.
-streamlit.dataframe(my_fruit_list)
+streamlit.dataframe(fruits_to_show)

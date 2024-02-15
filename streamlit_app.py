d8be0794573a607217f23431da80424ebbca8537
Isabel Uribe <87680316+IsaUribeA@users.noreply.github.com>
2024-02-15 17:37:24 -0600

    Update streamlit_app.py
    added a text entry box so that people can search for the fruit they want to research

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,6 +30,11 @@ streamlit.dataframe(fruits_to_show)
 #new section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 
+#add a text extry box and send the input to fruityvice as part of the api call
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
+
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 #streamlit.text(fruityvice_response.json()) #just writes the data on the screen
@@ -38,8 +43,6 @@ fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
 fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 # outputs the screen as a table
 streamlit.dataframe(fruityvice_normalized)
-
-
 
 
 

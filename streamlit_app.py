797e22394d5a74666a2b9a6f5923ed884055c2e2
Isabel Uribe <87680316+IsaUribeA@users.noreply.github.com>
2024-02-15 16:24:50 -0600

    Update streamlit_app.py
    header for the new section displayig fruityvice api response

--- streamlit_app.py
+++ streamlit_app.py
@@ -27,6 +27,9 @@ streamlit.dataframe(fruits_to_show)
 #Lesson 9 snowflake course
 #importing another pyhton package library called request
 
+#new section to display fruityvice api response
+streamlit.header("Fruityvice Fruit Advice!")
+
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response)

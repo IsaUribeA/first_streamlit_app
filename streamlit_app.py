757e9cf8466b97a502c2520fc4d64d44b8da3259
Isabel Uribe <87680316+IsaUribeA@users.noreply.github.com>
2024-02-19 14:08:04 -0600

    Update streamlit_app.py
    Move the Fruityvice Code into a Try-Except (with a nested If-Else)
    Introducing this structure allows us to separate the code that is loaded once from the code that should be repeated each time a new value is entered.

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,23 +30,22 @@ streamlit.dataframe(fruits_to_show)
 #Lesson 9 snowflake course
 #importing another pyhton package library called request
 
-#new section to display fruityvice api response
+#########new section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
-
-
+try:
 #add a text extry box and send the input to fruityvice as part of the api call
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
-
-
-#import requests
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
-#streamlit.text(fruityvice_response.json()) #just writes the data on the screen
-
-#take the raw json version of the response and normalize it, it models it as a table
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# outputs the screen as a table
-streamlit.dataframe(fruityvice_normalized)
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+      streamlit.error("Please select a fruit to get information.")
+  else:
+      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
+      #streamlit.text(fruityvice_response.json()) #just writes the data on the screen
+      #take the raw json version of the response and normalize it, it models it as a table
+      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+      # outputs the screen as a table
+      streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+    streamlit.error()
 
 # don't run anything past here while we troubleshoot
 streamlit.stop()

6ecab5c4577bcdbfd5d2855be955a0be5da9cebc
Isabel Uribe <87680316+IsaUribeA@users.noreply.github.com>
2024-02-19 14:57:24 -0600

    Update streamlit_app.py
    closed the snowflake connection on each button its opened when used

--- streamlit_app.py
+++ streamlit_app.py
@@ -74,9 +74,10 @@ def get_fruit_load_list():
         return my_cur.fetchall()
     
 #add a button to load the fruit
-if streamlit.button('Get Fruit Load List'):
+if streamlit.button('View Our Fruit List - Add Your Favorites!'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
+    my_cnx.close() #se abre la conexion a snowflake cuando se da click al boton, hay que cerrar esa conexion al final del script del boton
     streamlit.dataframe(my_data_rows)
 
 #allow the user to add a fruit to the list
@@ -89,6 +90,7 @@ add_my_fruit = streamlit.text_input('What fruit would you like to add?')
 if streamlit.button('Add a Fruit to the List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     back_from_function = insert_row_snowflake(add_my_fruit)
+    my_cnx.close()
     streamlit.text(back_from_function)
 
 

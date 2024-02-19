fdd4f8f3e50e1be1ca85b0feaf96007bcc322b54
Isabel Uribe <87680316+IsaUribeA@users.noreply.github.com>
2024-02-19 14:17:49 -0600

    Update streamlit_app.py
    created a function named get_fruityvice_data and entered in it some code we had before

--- streamlit_app.py
+++ streamlit_app.py
@@ -30,7 +30,15 @@ streamlit.dataframe(fruits_to_show)
 #Lesson 9 snowflake course
 #importing another pyhton package library called request
 
-#########new section to display fruityvice api response
+
+#create the repeatable code block called a function
+def get_fruityvice_data(this_fruit_choice):
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + this_fruit_choice)
+    #take the raw json version of the response and normalize it, it models it as a table
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    return fruityvice_normalized
+  
+######new section to display fruityvice api response
 streamlit.header("Fruityvice Fruit Advice!")
 try:
 #add a text extry box and send the input to fruityvice as part of the api call
@@ -38,12 +46,10 @@ try:
   if not fruit_choice:
       streamlit.error("Please select a fruit to get information.")
   else:
-      fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + "kiwi")
       #streamlit.text(fruityvice_response.json()) #just writes the data on the screen
-      #take the raw json version of the response and normalize it, it models it as a table
-      fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
       # outputs the screen as a table
-      streamlit.dataframe(fruityvice_normalized)
+      back_from_function = get_fruityvice_data(fruit_choice)
+      streamlit.dataframe(back_from_function)
 except URLError as e:
     streamlit.error()
 

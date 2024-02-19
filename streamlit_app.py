ec9fd4bc5bb1740c8006f1ca4814d36df628aa13
Isabel Uribe <87680316+IsaUribeA@users.noreply.github.com>
2024-02-19 14:44:13 -0600

    Update streamlit_app.py
    corrigiendo errores, faltaba quita el stop, por eso no aparecia un boton
    y agregando lo que faltaba de codigo para que el usuario pueda agrergar frutas a la tabla

--- streamlit_app.py
+++ streamlit_app.py
@@ -53,9 +53,6 @@ try:
 except URLError as e:
     streamlit.error()
 
-# don't run anything past here while we troubleshoot
-streamlit.stop()
-
 #the line shown below will tell your app to bring in some code from the snowflake library you added (snowflake-connector-python). 
 #import snowflake.connector
 
@@ -75,19 +72,26 @@ def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
         my_cur.execute("select * from fruit_load_list")
         return my_cur.fetchall()
-
+    
 #add a button to load the fruit
 if streamlit.button('Get Fruit Load List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
     streamlit.dataframe(my_data_rows)
 
-#adding a second text entry box
-#add a text extry box and send the input to fruityvice as part of the api call
-add_my_fruit = streamlit.text_input('What fruit would you like to add?','Jackfruit')
-streamlit.write('Thanks for adding ', add_my_fruit)
+#allow the user to add a fruit to the list
+def insert_row_snowflake(new_fruit):
+    with my_cnx.cursor() as my_cur:
+        my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+        return "Thanks for adding " + new_fruit
+        
+add_my_fruit = streamlit.text_input('What fruit would you like to add?')
+if streamlit.button('Add a Fruit to the List'):
+    my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+    back_from_function = insert_row_snowflake(add_my_fruit)
+    streamlit.text(back_from_function)
 
-my_cur.execute("insert into fruit_load_list values ('from streamlit')")
+
 
 
 

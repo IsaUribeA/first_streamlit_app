5117213965327bc710bb67237eab9f57e5ce77ba
Isabel Uribe <87680316+IsaUribeA@users.noreply.github.com>
2024-02-19 14:58:23 -0600

    Update streamlit_app.py
    changed the name of the title and button

--- streamlit_app.py
+++ streamlit_app.py
@@ -66,7 +66,7 @@ except URLError as e:
 
 #querying some data 
 #Move the Fruit Load List Query and Load into a Button Action
-streamlit.header("The fruit load list contains:")
+streamlit.header("View Our Fruit List - Add Your Favorites!")
 #snowflake-related functions
 def get_fruit_load_list():
     with my_cnx.cursor() as my_cur:
@@ -74,7 +74,7 @@ def get_fruit_load_list():
         return my_cur.fetchall()
     
 #add a button to load the fruit
-if streamlit.button('View Our Fruit List - Add Your Favorites!'):
+if streamlit.button('Get Fruit List'):
     my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
     my_data_rows = get_fruit_load_list()
     my_cnx.close() #se abre la conexion a snowflake cuando se da click al boton, hay que cerrar esa conexion al final del script del boton
